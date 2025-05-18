7b57bb027f79194d3b5edf0ee148dfa570d836f2
Cansu Zeynep <cansu.zeynep.kars88@gmail.com>
2025-05-18 19:06:07 +0300

    Added the brain icons and did some HTML/CSS changes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, url_for, request, session, redirect, flash, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
 from flask_login import *
 from helpers import *
